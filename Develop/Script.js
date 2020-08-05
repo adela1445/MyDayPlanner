9732d80a21af24cfe08825509fec21c60272a02a
Bluebird <stephany1445@hotmail.com>
2020-08-04 23:19:53 -0700

    Added current dat to html using jquery with moment

--- Develop/Script.js
+++ Develop/Script.js
@@ -1,11 +1,8 @@
-// Makes sure that the js doesnt get run until the HTML file is finished loading
-$(document).ready(function () {
-  // Console logging to see js is connected to html
-  console.log("Hi, Checking if you are linked.");
+// Console logging to see js is connected to html
+console.log("Hi, Checking if you are linked.");
 
-  // Trying to capture current date using Moment
+// Trying to capture current date using Moment
 
-  var moment = require("moment");
-  moment().format();
-  console.log(moment);
-});
+var moment = moment().format("dddd, MMM Do YYYY");
+console.log(moment);
+var currentDate = $("#currentDay").text(moment);
