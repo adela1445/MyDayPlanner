97ca506dea6eda687134a9a19a0ef558c1a20039
Bluebird <stephany1445@hotmail.com>
2020-08-04 18:33:08 -0700

    added code in js file

--- Develop/Script.js
+++ Develop/Script.js
@@ -1,1 +1,11 @@
-console.log("Hi, Checking if you are linked.");
+// Makes sure that the js doesnt get run until the HTML file is finished loading
+$(document).ready(function () {
+  // Console logging to see js is connected to html
+  console.log("Hi, Checking if you are linked.");
+
+  // Trying to capture current date using Moment
+
+  var moment = require("moment");
+  moment().format();
+  console.log(moment);
+});
