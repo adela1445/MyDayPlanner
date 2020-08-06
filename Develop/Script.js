5f3e9cef6bff29a6a3c272c461ea1db188ef56ab
Bluebird <stephany1445@hotmail.com>
2020-08-05 17:11:24 -0700

    added new id to html removed non unique id to avoid future conflict/issue created var to ensure the current hour is being captureed as a number created var to ensure the work hour is bein captured as a number created var for each event input box to grab value

--- Develop/Script.js
+++ Develop/Script.js
@@ -3,6 +3,28 @@ console.log("Hi, Checking if you are linked.");
 
 // Trying to capture current date using Moment
 
-var moment = moment().format("dddd, MMM Do YYYY");
-console.log(moment);
-var currentDate = $("#currentDay").text(moment);
+var momentdata = moment().format("dddd, MMM Do YYYY");
+console.log(momentdata);
+var currentDate = $("#currentDay").text(momentdata);
+
+// Creating variables to ensure hour is being compared to a number
+
+var hour = parseInt(moment().format("h"));
+var workHour = parseInt($(".hour").text());
+
+console.log(hour);
+console.log(workHour);
+
+var eventInput9am = $("#event9Title").val();
+var eventInput10am = $("#event10Title").val();
+var eventInput11am = $("#event11Title").val();
+var eventInput12pm = $("#event12Title").val();
+var eventInput1pm = $("#event1Title").val();
+var eventInput2pm = $("#event2Title").val();
+var eventInput3pm = $("#event3Title").val();
+var eventInput4pm = $("#event4Title").val();
+var eventInput5pm = $("#event4Title").val();
+
+$("button").on("click", function () {
+  event.preventDefault();
+});
