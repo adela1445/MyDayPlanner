b38eebb0103c5bece80d6316d3b459975d89d7d1
Bluebird <stephany1445@hotmail.com>
2020-08-05 18:30:30 -0700

    added variable to local storage

--- Develop/Script.js
+++ Develop/Script.js
@@ -9,10 +9,10 @@ var currentDate = $("#currentDay").text(momentdata);
 
 // Creating variables to ensure hour is being compared to a number
 
-var hour = parseInt(moment().format("h"));
+var atmHour = parseInt(moment().format("h"));
 var workHour = parseInt($(".hour").text());
 
-console.log(hour);
+console.log(atmHour);
 console.log(workHour);
 
 var eventInput9am = $("#event9Title").val();
@@ -23,8 +23,40 @@ var eventInput1pm = $("#event1Title").val();
 var eventInput2pm = $("#event2Title").val();
 var eventInput3pm = $("#event3Title").val();
 var eventInput4pm = $("#event4Title").val();
-var eventInput5pm = $("#event4Title").val();
+var eventInput5pm = $("#event5Title").val();
+
+// Creating an if statement so we can compare the to var in order to know if the time is present past or future
+if (workHour === atmHour) {
+  $("input").addClass("present");
+} else if (workHour < atmHour) {
+  $("input").addClass("past");
+} else {
+  $("input").addClass("future");
+}
 
 $("button").on("click", function () {
   event.preventDefault();
+
+  // Creating variable to store into local Storage
+  var save9 = eventInput9am.text;
+  var save10 = eventInput10am.value;
+  var save11 = eventInput11am.value;
+  var save12 = eventInput12pm.value;
+  var save1 = eventInput1pm.value;
+  var save2 = eventInput2pm.value;
+  var save3 = eventInput3pm.value;
+  var save4 = eventInput4pm.value;
+  var save5 = eventInput5pm.value;
+
+  // Adding variables to localStorage
+
+  localStorage.setItem("nineEvent", JSON.stringify(save9));
+  localStorage.setItem("tenEvent", JSON.stringify(save10));
+  localStorage.setItem("elevenEvent", JSON.stringify(save11));
+  localStorage.setItem("twelveEvent", JSON.stringify(save12));
+  localStorage.setItem("oneEvent", JSON.stringify(save1));
+  localStorage.setItem("twoEvent", JSON.stringify(save2));
+  localStorage.setItem("threeEvent", JSON.stringify(save3));
+  localStorage.setItem("FourEvent", JSON.stringify(save4));
+  localStorage.setItem("FiveEvent", JSON.stringify(save5));
 });
